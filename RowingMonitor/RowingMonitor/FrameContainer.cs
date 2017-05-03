5ffc1b1c351418ae44caa713f4592e36d633f5e8
Sven Otte <svenotte@live.de>
2017-05-03 16:31:10 +0200

    INIT: OSP Rowing Analysis project

---
+++ RowingMonitor/RowingMonitor/FrameContainer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Kinect;
+using System.Diagnostics;
+
+namespace RowingMonitor
+{
+    class FrameContainer
+    {
+        public struct Frame
+        {
+            public int index;
+            public IReadOnlyDictionary<JointType, Joint> rawJoints;
+            public IReadOnlyDictionary<JointType, Joint> smoothedJoints;
+        }
+
+        List<Frame> frames = new List<Frame>();
+
+        public void AddFrame(IReadOnlyDictionary<JointType, Joint> joints)
+        {
+            Frame f = new Frame();
+            f.rawJoints = joints;
+            frames.Add(f);
+
+            Debug.WriteLine("chip: " + joints[JointType.SpineBase].Position.X + ", "
+                                + joints[JointType.SpineBase].Position.Y + ", "
+                                + joints[JointType.SpineBase].Position.Z);
+        }
+
+        public IReadOnlyDictionary<JointType, Joint> GetFrameRaw(int index)
+        {
+            return frames[index].rawJoints;
+        }
+
+        public List<Frame> GetAllFramesRaw()
+        {
+            return frames;
+        }
+    }
+}
---
+++ RowingMonitor/RowingMonitor/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Kinect;
+using OxyPlot;
+using OxyPlot.Series;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RowingMonitor
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            // bind view model to view
+            this.DataContext = new MainViewModel();           
+
+            // initialize the components (controls) of the window
+            this.InitializeComponent();
+        }
+    }
+}
